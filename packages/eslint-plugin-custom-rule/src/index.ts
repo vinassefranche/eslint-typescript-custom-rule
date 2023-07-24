5443aa685e262388041afc4298a4f6f2cdae1602
vinassefranche <vincent.francois@inato.com>
2023-07-24 14:30:29 +0200

    Help typescript understand rules types

--- packages/eslint-plugin-custom-rule/src/index.ts
+++ packages/eslint-plugin-custom-rule/src/index.ts
@@ -1,5 +1,6 @@
+import { TSESLint } from '@typescript-eslint/utils';
 import myRule from './myRule';
 
 export const rules = {
   'my-rule': myRule,
-};
+} satisfies Record<string, TSESLint.RuleModule<string, Array<unknown>>>;
